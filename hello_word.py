c8f6c6ab9351ace349d9692508dec1413ede4912
christopher <coronac511@gmail.com>
2024-11-12 18:05:24 -0500

    first commit

---
+++ hello_word.py
@@ -0,0 +1,6 @@
+import my_module
+
+
+my_module.greet("Juan") 
+result = my_module.calculate_sum(5, 3)
+print(result) 
---
+++ my_module.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+def calculate_sum(a, b):
+    return a + b
