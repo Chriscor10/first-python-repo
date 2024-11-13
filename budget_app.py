b0ee9da8eff6bed14dcc356ecaa067778242d597
christopher <coronac511@gmail.com>
2024-11-12 19:43:26 -0500

    added functions to save data on json file

--- budget_app.py
+++ budget_app.py
@@ -1,3 +1,4 @@
+import json
 
 def add_expense(expenses, description, amount):
     expenses.append({"description": description, "amount": amount})
@@ -16,14 +17,32 @@ def show_budget_details(budget, expenses):
         print(f"- {expense['description']}: {expense['amount']}")
     print(f"Total Spent: {get_total_expenses(expenses)}")
     print(f"Remaining Budget: {get_balance(budget, expenses)}")
-    
+
+def load_budget_data(filepath):
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            return data['initial_budget'], data['expenses']
+    except (FileNotFoundError, json.JSONDecodeError):
+        return 0, []  # Return default values if the file doesn't exist or is empty/corrupted
+#
+def save_budget_data(filepath, initial_budget, expenses):
+    data = {
+        'initial_budget': initial_budget,
+        'expenses': expenses
+    }
+    with open(filepath, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
 def main():
     print("Welcome to the Budget App")
-    initial_budget = float(input("Please enter your initial budget: "))
-    # filepath = 'budget_data.json'  # Define the path to your JSON file
-    # initial_budget, expenses = load_budget_data(filepath)
+    filepath = 'budget_data.json'  # Define the path to your JSON file
+    initial_budget, expenses = load_budget_data(filepath)
+    if initial_budget == 0:
+        initial_budget = float(input("Please enter your initial budget: "))
     budget = initial_budget
-    expenses = []
+   
 
     while True:
         print("\nWhat would you like to do?")
@@ -39,6 +58,7 @@ def main():
         elif choice == "2":
             show_budget_details(budget, expenses)
         elif choice == "3":
+            save_budget_data(filepath,initial_budget, expenses)
             print("Exiting Budget App. Goodbye!")
             break
         else:
